f44e306a7c5e518ae3394be20da2e04a1dbeb304
Nick Yashin <nyashin92@gmail.com>
2020-03-02 11:53:59 +0100

    uncoment hook update files

--- client/gql.d.ts
+++ client/gql.d.ts
@@ -1,50 +1,50 @@
 export type Maybe<T> = T | null;
 /** All built-in and custom scalars, mapped to their actual values */
-export type Scalars = {
-  ID: string,
-  String: string,
-  Boolean: boolean,
-  Int: number,
-  Float: number,
-  Upload: any,
-};
+export interface Scalars {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  Upload: any;
+}
 
 export enum GQLCacheControlScope {
   Public = 'PUBLIC',
   Private = 'PRIVATE'
 }
 
-export type GQLMutation = {
-   __typename?: 'Mutation',
-  simple: GQLSimpleType,
-};
+export interface GQLMutation {
+  __typename?: 'Mutation';
+  simple: GQLSimpleType;
+}
 
 
-export type GQLMutationSimpleArgs = {
-  value: Scalars['String']
-};
+export interface GQLMutationSimpleArgs {
+  value: Scalars['String'];
+}
 
-export type GQLQuery = {
-   __typename?: 'Query',
-  simple: GQLSimpleType,
-};
-
-
-export type GQLQuerySimpleArgs = {
-  id: Scalars['Int'],
-  value?: Maybe<Scalars['String']>
-};
-
-export type GQLSimpleType = {
-   __typename?: 'SimpleType',
-  value: Scalars['String'],
-  id: Scalars['Int'],
-};
+export interface GQLQuery {
+  __typename?: 'Query';
+  simple: GQLSimpleType;
+}
 
 
-export type GQLInlineMutationVariables = {
-  value: Scalars['String']
-};
+export interface GQLQuerySimpleArgs {
+  id: Scalars['Int'];
+  value?: Maybe<Scalars['String']>;
+}
+
+export interface GQLSimpleType {
+  __typename?: 'SimpleType';
+  value: Scalars['String'];
+  id: Scalars['Int'];
+}
+
+
+export interface GQLInlineMutationVariables {
+  value: Scalars['String'];
+}
 
 
 export type GQLInlineMutation = (
@@ -52,13 +52,13 @@ export type GQLInlineMutation = (
   & { simple: (
     { __typename?: 'SimpleType' }
     & Pick<GQLSimpleType, 'id' | 'value'>
-  ) }
+  ); }
 );
 
-export type GQLExternalQueryVariables = {
-  id: Scalars['Int'],
-  value?: Maybe<Scalars['String']>
-};
+export interface GQLExternalQueryVariables {
+  id: Scalars['Int'];
+  value?: Maybe<Scalars['String']>;
+}
 
 
 export type GQLExternalQuery = (
@@ -66,5 +66,5 @@ export type GQLExternalQuery = (
   & { simple: (
     { __typename?: 'SimpleType' }
     & Pick<GQLSimpleType, 'id' | 'value'>
-  ) }
+  ); }
 );
